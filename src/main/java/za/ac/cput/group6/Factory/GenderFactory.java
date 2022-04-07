1dc8860df09a12814a30786e0dbbb48b5092413b
OkuhleNdlebe <218338619@mycput.ac.za>
2022-04-08 00:06:50 +0200

    initial commit

---
+++ src/main/java/za/ac/cput/group6/Factory/GenderFactory.java
@@ -0,0 +1,4 @@
+package za.ac.cput.group6.Factory;
+
+public class GenderFactory {
+}
---
+++ src/test/java/za/ac/cput/group6/Factory/GenderFactoryTest.java
@@ -0,0 +1,7 @@
+package za.ac.cput.group6.Factory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenderFactoryTest {
+
+}
