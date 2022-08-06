0e3ade64fc0949dd80d0e46725cdca1804988831
OkuhleNdlebe <73134423+OkuhleNdlebe@users.noreply.github.com>
2022-08-06 18:15:49 +0200

    Delete src/main/java/za/ac/cput/group6 directory

--- src/main/java/za/ac/cput/group6/Repository/Donor/IDonorRepository.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.group6.Repository.Donor;
-
-import za.ac.cput.group6.Entity.Donor;
-import za.ac.cput.group6.Repository.IRepository;
-import java.util.Set;
-
-public interface IDonorRepository extends IRepository<Donor,String> {
-    public Set<Donor> getAll();
-}
--- src/main/java/za/ac/cput/group6/Util/GenericHelper.java
+++
@@ -1,4 +0,0 @@
-package za.ac.cput.group6.Util;
-
-public class GenericHelper {
-}
