ab3f1065f59e771f338b9b556ebc2a8a012e48e3
Lukhona <lukhonatetyana@gmail.com>
2022-08-17 21:57:26 +0200

    Fixing commit

--- src/main/java/za/ac/cput/group6/domain/lookup/Address.java
+++ src/main/java/za/ac/cput/group6/domain/lookup/Address.java
@@ -1,4 +1,75 @@
 package za.ac.cput.group6.domain.lookup;
 
 public class Address {
+    
+    private String streetNumber;
+    private String streetName;
+    private int postalCode;
+
+    private Address(){
+        
+    }
+
+    public Address(Builder builder) {
+        this.streetNumber= builder.streetNumber;
+        this.streetName= builder.streetName;
+        this.postalCode= builder.postalCode;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "streetNumber='" + streetNumber + '\'' +
+                ", streetName='" + streetName + '\'' +
+                ", postalCode=" + postalCode +
+                '}';
+    }
+    public static class Builder{
+        public String donorId;
+        public Address address;
+        private String streetNumber, streetName;
+        private int postalCode;
+
+        public Builder setStreetNumber(String streetNumber) {
+            this.streetNumber = streetNumber;
+            
+            return this;
+        }
+
+        public Builder setStreetName(String streetName) {
+            this.streetName = streetName;
+            
+            return this;
+        }
+
+        public Builder setPostalCode(int postalCode) {
+            this.postalCode = postalCode;
+            
+            return this;
+        }
+        public Address build(){return new Address(this);}
+        
+        public Builder copy(Address address){
+            this.streetNumber = address.streetNumber;
+            this.streetName = address.streetName;
+            this.postalCode = address.postalCode;
+            
+            return this;
+            
+        }
+    }
+    
 }
+
--- src/main/java/za/ac/cput/group6/domain/user/DonorAddress.java
+++ src/main/java/za/ac/cput/group6/domain/user/DonorAddress.java
@@ -1,4 +1,59 @@
 package za.ac.cput.group6.domain.user;
 
+import za.ac.cput.group6.domain.lookup.Address;
+
 public class DonorAddress {
+    private  String donorId;
+    private Address address;
+
+    private DonorAddress() {
+    }
+
+    public DonorAddress(Address.Builder builder){
+        this.donorId= builder.donorId;
+        this.address= builder.address;
+    }
+
+    public DonorAddress(Builder builder) {
+    }
+
+    public String getDonorId() {
+        return donorId;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "DonorAddress{" +
+                "donorId='" + donorId + '\'' +
+                ", address=" + address +
+                '}';
+    }
+     public static class Builder{
+        private String donorId;
+        private Address address;
+
+         public Builder setDonorId(String donorId) {
+             this.donorId = donorId;
+
+             return this;
+         }
+
+         public Builder setAddress(Address address) {
+             this.address = address;
+
+             return this;
+         }
+         public DonorAddress build(){return new DonorAddress(this);}
+
+         public Builder copy(DonorAddress donorAddress){
+             this.donorId= donorAddress.donorId;
+             this.address= donorAddress.address;
+
+             return this;
+         }
+     }
 }
