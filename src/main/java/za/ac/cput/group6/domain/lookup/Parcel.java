ce97e9676ff1ae7c6cf26eb6d4050754eafad58c
Justin Nkuna <219319820@mycput.ac.za>
2022-08-23 23:20:42 +0200

    Parcel and Request Domain

--- src/main/java/za/ac/cput/group6/domain/lookup/Parcel.java
+++ src/main/java/za/ac/cput/group6/domain/lookup/Parcel.java
@@ -1,4 +1,68 @@
 package za.ac.cput.group6.domain.lookup;
 
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+
 public class Parcel {
-}
+
+    private String parcelId;
+    private String parcelDescription;
+    private String date;
+    private String parcelType;
+    private String parcelStatus;
+
+
+
+    public Parcel() {
+
+    }
+
+
+
+    public Parcel(Builder builder) {
+        this.parcelId = builder.parcelId;
+        this.parcelDescription = builder.parcelDescription;
+        this.date = builder.date;
+        this.parcelType = builder.parcelType;
+        this.parcelStatus = builder.parcelStatus;
+
+    }
+
+    public Parcel(Request request) {
+    }
+
+
+    @Override
+    public String toString() {
+        return "Parcel{" +
+                "parcelId='" + parcelId + '\'' +
+                ", parcelDescription='" + parcelDescription + '\'' +
+                ", date='" + date + '\'' +
+                ", parcelType='" + parcelType + '\'' +
+                ", parcelStatus='" + parcelStatus + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+            private String parcelId;
+            private String parcelDescription;
+            private String date;
+            private String parcelType;
+            private String parcelStatus;
+
+
+        }
+
+        public Parcel copy(Parcel parcel){
+
+            this.parcelId=parcel.parcelId;
+            this.parcelDescription =parcel.parcelDescription;
+            this.date=parcel.date;
+            this.parcelType=parcel.parcelType;
+            this.parcelStatus=parcel.parcelStatus;
+            return this;
+        }
+    }
+
